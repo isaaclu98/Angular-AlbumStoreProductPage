ed9e451c84f25d1db35850bf0efc0637e5ba7fc4
Isaac Lu <Isaac.Lu@bnymellon.com>
2020-12-16 10:33:53 -0500

    Module 3 completed added interface/classes for Album and Track Objects

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,18 +1,18 @@
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Http , Response} from '@angular/http';
+import { Album } from './album';
 import 'rxjs/add/operator/map';
 
 
 @Injectable()
 export class ProductService {
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) { }
 
-  private _albumUrl : string = "../assets/album.json";
-
-  getAlbum(id : number){
-    return this._http.get(this._albumUrl).map((response) => response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album> response.json());
   }
 }
